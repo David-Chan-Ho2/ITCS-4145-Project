813ac415191c9444ce3a0f05eb1b5fe16330a1b3
David-Chan-Ho2 <dho12@charlotte.edu>
2026-06-12 19:02:59 -0400

    modify count-sort to use input files

--- OpenMP/serial/count_sort.c
+++ OpenMP/serial/count_sort.c
@@ -3,15 +3,13 @@
  * Purpose: Use count_sort sort to sort a list of ints.
  *
  * Compile: gcc -g -Wall -o count_sort count_sort.c
- * Usage:   count_sort <n> <g|i>
- *             n:   number of elements in list
- *            'g':  generate list using a random number generator
- *            'i':  user input list
+ * Usage:   count_sort <input_file>
+ *             input_file: text file with one integer per line
  *
- * Input:   list (optional)
+ * Input:   input file
  * Output:  sorted list
  *
- * IPP:
+ * IPP: Programming Problem 5.3 (p. 287)
  */
 
 #include <stdlib.h>
@@ -19,14 +17,9 @@
 #include <stdio.h>
 #include <time.h>
 
-/* For random list, 0 <= keys < RMAX */
-const int RMAX = 100;
-
 void Usage(char *prog_name);
-void Get_args(int argc, char *argv[], int *n_p, char *g_i_p);
-void Generate_list(int a[], int n);
 void Print_list(int a[], int n, char *title);
-void Read_list(int a[], int n);
+void Read_list(FILE *fp, int a[], int n);
 void Calculate_elapsed(clock_t start_time, clock_t end_time);
 void Count_sort(int a[], int n);
 
@@ -34,21 +27,38 @@ void Count_sort(int a[], int n);
 int main(int argc, char *argv[])
 {
     int n;
-    char g_i;
     int *a;
     clock_t start_time, end_time;
+    FILE *fp;
 
-    Get_args(argc, argv, &n, &g_i);
+    if (argc != 2)
+    {
+        Usage(argv[0]);
+        exit(0);
+    }
+
+    char filepath[512];
+    snprintf(filepath, sizeof(filepath), "../../input-files/%s", argv[1]);
+    fp = fopen(filepath, "r");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Error: cannot open file %s\n", filepath);
+        exit(1);
+    }
+
+    /* count lines to determine n */
+    int ch;
+    n = 0;
+    while ((ch = fgetc(fp)) != EOF)
+        if (ch == '\n')
+            n++;
+    rewind(fp);
+
     a = (int *)malloc(n * sizeof(int));
-    if (g_i == 'g')
-    {
-        Generate_list(a, n);
-        Print_list(a, n, "Before sort");
-    }
-    else
-    {
-        Read_list(a, n);
-    }
+    Read_list(fp, a, n);
+    fclose(fp);
+
+    Print_list(a, n, "Before sort");
 
     start_time = clock();
     Count_sort(a, n);
@@ -67,49 +77,9 @@ int main(int argc, char *argv[])
  */
 void Usage(char *prog_name)
 {
-    fprintf(stderr, "usage:   %s <n> <g|i>\n", prog_name);
-    fprintf(stderr, "   n:   number of elements in list\n");
-    fprintf(stderr, "  'g':  generate list using a random number generator\n");
-    fprintf(stderr, "  'i':  user input list\n");
+    fprintf(stderr, "usage:   %s <input_file>\n", prog_name);
+    fprintf(stderr, "   input_file: text file with one integer per line\n");
 } /* Usage */
-
-/*-----------------------------------------------------------------
- * Function:  Get_args
- * Purpose:   Get and check command line arguments
- * In args:   argc, argv
- * Out args:  n_p, g_i_p
- */
-void Get_args(int argc, char *argv[], int *n_p, char *g_i_p)
-{
-    if (argc != 3)
-    {
-        Usage(argv[0]);
-        exit(0);
-    }
-    *n_p = atoi(argv[1]);
-    *g_i_p = argv[2][0];
-
-    if (*n_p <= 0 || (*g_i_p != 'g' && *g_i_p != 'i'))
-    {
-        Usage(argv[0]);
-        exit(0);
-    }
-} /* Get_args */
-
-/*-----------------------------------------------------------------
- * Function:  Generate_list
- * Purpose:   Use random number generator to generate list elements
- * In args:   n
- * Out args:  a
- */
-void Generate_list(int a[], int n)
-{
-    int i;
-
-    srandom(0);
-    for (i = 0; i < n; i++)
-        a[i] = random() % RMAX;
-} /* Generate_list */
 
 /*-----------------------------------------------------------------
  * Function:  Print_list
@@ -128,17 +98,16 @@ void Print_list(int a[], int n, char *title)
 
 /*-----------------------------------------------------------------
  * Function:  Read_list
- * Purpose:   Read elements of list from stdin
- * In args:   n
+ * Purpose:   Read elements of list from file
+ * In args:   fp, n
  * Out args:  a
  */
-void Read_list(int a[], int n)
+void Read_list(FILE *fp, int a[], int n)
 {
     int i;
 
-    printf("Please enter the elements of the list\n");
     for (i = 0; i < n; i++)
-        scanf("%d", &a[i]);
+        fscanf(fp, "%d", &a[i]);
 } /* Read_list */
 
 /*-----------------------------------------------------------------
